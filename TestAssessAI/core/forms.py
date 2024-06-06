f27ab7be04322837a78cf5227c7306e3a89dbcb5
Ratmir Gaitov <gaitovrat@gmail.com>
2024-06-06 18:54:04 +0200

    Fix question saving

--- TestAssessAI/core/forms.py
+++ TestAssessAI/core/forms.py
@@ -2,10 +2,12 @@ from django import forms
 from .models import Question, Test, Submission
 from ckeditor.widgets import CKEditorWidget
 
+
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['question_text', 'score']
+
 
 class QuestionForm(forms.ModelForm):
     class Meta:
@@ -14,11 +16,13 @@ class QuestionForm(forms.ModelForm):
         widgets = {
             'question_text': CKEditorWidget(),
         }
-        
+
+
 class TestForm(forms.ModelForm):
     class Meta:
         model = Test
-        fields = ['test_title', 'test_description', 'start_time', 'end_time', 'duration', 'max_score']
+        fields = ['test_title', 'test_description',
+                  'start_time', 'end_time', 'duration', 'max_score']
         widgets = {
             'start_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
             'end_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
@@ -32,14 +36,17 @@ class TestForm(forms.ModelForm):
 
         if start_time and end_time:
             if start_time >= end_time:
-                raise forms.ValidationError("End time must be after start time.")
+                raise forms.ValidationError(
+                    "End time must be after start time.")
 
         if start_time and duration:
             if duration <= 0:
-                raise forms.ValidationError("Duration must be a positive number.")
+                raise forms.ValidationError(
+                    "Duration must be a positive number.")
 
         return cleaned_data
-    
+
+
 class TestSubmissionForm(forms.ModelForm):
     class Meta:
         model = Submission
--- TestAssessAI/core/views.py
+++ TestAssessAI/core/views.py
@@ -8,6 +8,9 @@ from django.urls import reverse
 from django.utils import timezone
 from django.db.models import Sum
 
+from dataclasses import dataclass
+
+
 @login_required
 def teacher_dashboard(request):
     if not request.user.is_teacher():
@@ -15,14 +18,17 @@ def teacher_dashboard(request):
     subjects = Subject.objects.filter(teacher=request.user)
     return render(request, 'core/teacher_dashboard.html', {'subjects': subjects})
 
+
 @login_required
 def add_subject(request):
     if request.method == 'POST' and request.user.is_teacher():
         title = request.POST.get('title')
         description = request.POST.get('description')
-        Subject.objects.create(lesson_title=title, description=description, teacher=request.user)
+        Subject.objects.create(
+            lesson_title=title, description=description, teacher=request.user)
         return redirect('teacher_dashboard')
     return render(request, 'core/add_subject.html')
+
 
 @login_required
 def delete_subject(request, subject_id):
@@ -33,6 +39,7 @@ def delete_subject(request, subject_id):
         except Subject.DoesNotExist:
             pass
     return redirect('teacher_dashboard')
+
 
 @login_required
 def edit_subject(request, subject_id):
@@ -49,6 +56,7 @@ def edit_subject(request, subject_id):
             pass
     return redirect('teacher_dashboard')
 
+
 @login_required
 def add_student_to_subject(request, subject_id):
     subject = get_object_or_404(Subject, pk=subject_id, teacher=request.user)
@@ -57,7 +65,7 @@ def add_student_to_subject(request, subject_id):
         selected_students = Student.objects.filter(id__in=student_ids)
         subject.students.set(selected_students)
         return redirect('view_subject', subject_id=subject_id)
-    
+
     User = get_user_model()
     all_students = User.objects.filter(role=User.Role.STUDENT)
     subject_students = subject.students.all()
@@ -67,6 +75,7 @@ def add_student_to_subject(request, subject_id):
         'subject_students': subject_students
     })
 
+
 @login_required
 def add_test_to_subject(request, subject_id):
     if request.method == 'POST' and request.user.is_teacher():
@@ -75,23 +84,25 @@ def add_test_to_subject(request, subject_id):
             test = form.save(commit=False)
             test.subject_id = subject_id
             test.save()
-            subject_url = reverse('view_subject', kwargs={'subject_id': subject_id})
+            subject_url = reverse('view_subject', kwargs={
+                                  'subject_id': subject_id})
             return redirect(subject_url)
     else:
         form = TestForm()
 
     return render(request, 'core/add_test_to_subject.html', {'form': form, 'subject_id': subject_id})
 
+
 @login_required
 def student_dashboard(request):
     if not request.user.is_student():
         return redirect('index')
-    
+
     now = timezone.localtime(timezone.now())
-    
+
     subjects = Subject.objects.filter(students=request.user)
     subject_tests = []
-    
+
     for subject in subjects:
         tests = Test.objects.filter(subject=subject)
         for test in tests:
@@ -99,12 +110,16 @@ def student_dashboard(request):
             test.save()
         total_score = 0
         for test in tests:
-            submissions = Submission.objects.filter(student=request.user, question__test=test)
-            total_score += submissions.aggregate(Sum('mark__teacher_mark'))['mark__teacher_mark__sum'] or 0
-        
-        subject_tests.append({'subject': subject, 'tests': tests, 'total_score': total_score})
-    
+            submissions = Submission.objects.filter(
+                student=request.user, question__test=test)
+            total_score += submissions.aggregate(Sum('mark__teacher_mark'))[
+                'mark__teacher_mark__sum'] or 0
+
+        subject_tests.append(
+            {'subject': subject, 'tests': tests, 'total_score': total_score})
+
     return render(request, 'core/student_dashboard.html', {'subject_tests': subject_tests, 'now': now})
+
 
 @login_required
 def view_subject(request, subject_id):
@@ -117,10 +132,12 @@ def view_subject(request, subject_id):
         return redirect('teacher_dashboard')
     return render(request, 'core/view_subject.html', {'subject': subject, 'students': all_students})
 
+
 @login_required
 def view_test(request, test_id):
     test = get_object_or_404(Test, pk=test_id)
     return render(request, 'core/view_test.html', {'test': test})
+
 
 @login_required
 def edit_test(request, test_id):
@@ -129,10 +146,11 @@ def edit_test(request, test_id):
         form = TestForm(request.POST, instance=test)
         if form.is_valid():
             form.save()
-            return redirect('view_subject', subject_id=test.subject.pk)  
+            return redirect('view_subject', subject_id=test.subject.pk)
     else:
         form = TestForm(instance=test)
     return render(request, 'core/edit_test.html', {'form': form, 'test': test})
+
 
 @login_required
 def delete_test(request, test_id):
@@ -140,6 +158,7 @@ def delete_test(request, test_id):
     subject_id = test.subject.subject_id
     test.delete()
     return redirect('view_subject', subject_id=subject_id)
+
 
 @login_required
 def add_question_to_test(request, test_id):
@@ -155,6 +174,7 @@ def add_question_to_test(request, test_id):
         form = QuestionForm()
     return render(request, 'core/add_question_to_test.html', {'form': form, 'test': test})
 
+
 @login_required
 def edit_question(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -167,6 +187,7 @@ def edit_question(request, question_id):
         form = QuestionForm(instance=question)
     return render(request, 'core/edit_question.html', {'form': form, 'question': question})
 
+
 @login_required
 def delete_question(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -174,23 +195,33 @@ def delete_question(request, question_id):
     question.delete()
     return redirect('view_test', test_id=test_id)
 
+
+@dataclass
+class Question:
+    question_id: int
+    question_anwser: str
+
+
 @login_required
 def view_test_student(request, test_id):
     test = Test.objects.get(pk=test_id)
     questions = test.question_set.all()
+    form_questions = []
 
     if request.method == 'POST':
-        form = TestSubmissionForm(request.POST)
-        if form.is_valid():
+        form_questions = [Question(question_id=int(key.split('question_answer_')[1]), question_anwser=value) for key,
+                          value in request.POST.items() if 'question_answer' in key]
+
+        for form_question in form_questions:
             student = request.user
-            for question in questions:
-                answer_text = form.cleaned_data.get(f'question_answer_{question.pk}')
-                submission = Submission.objects.create(
-                    question_answer=answer_text,
-                    student=student,
-                    question=question
-                )
-            return redirect('student_dashboard')
+            question = next(
+                (question for question in questions if question.pk == form_question.question_id), None)
+            submission = Submission.objects.create(
+                question_answer=form_question.question_anwser,
+                student=student,
+                question=question
+            )
+        return redirect('student_dashboard')
     else:
         form = TestSubmissionForm()
 
